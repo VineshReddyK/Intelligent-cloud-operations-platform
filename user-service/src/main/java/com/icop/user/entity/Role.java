3cff8179e9bfc1d394ef4830d32c355459a7024a
Vinesh Reddy Kankanalapally <vineshreddyy.k@gmail.com>
2026-07-14 09:54:24 -0500

    updated code

--- user-service/src/main/java/com/icop/user/entity/Role.java
+++ user-service/src/main/java/com/icop/user/entity/Role.java
@@ -1,5 +1,7 @@
 package com.icop.user.entity;
 
+// two roles is all this needs for now. stored by name (see User.role),
+// so adding entries here later is safe — just don't rename existing ones
 public enum Role {
     USER, ADMIN
 }
